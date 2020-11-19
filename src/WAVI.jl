145fc4acb188eec2b104b1c5d262dfbf1871b7ee
RJArthern <rart@bas.ac.uk>
2020-11-19 12:24:00 +0000

    Make WAVI use MKL BLAS libraries

--- src/WAVI.jl
+++ src/WAVI.jl
@@ -2,7 +2,7 @@ module WAVI
 
 #Useful packages
 using LinearAlgebra, SparseArrays, LinearMaps, Parameters,
-      IterativeSolvers, Interpolations, BenchmarkTools, PyPlot
+      IterativeSolvers, Interpolations, BenchmarkTools, PyPlot, MKLSparse
 
 #Import functions so they can be modified in this module.
 import LinearAlgebra: ldiv!
