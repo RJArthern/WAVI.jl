382edeb96fc11f397b24d79e2eb3f099efb67eb1
Alex Bradley <36222451+alextbradley@users.noreply.github.com>
2021-04-30 15:44:16 +0100

    updated glx method to grid spec

--- src/WAVI.jl
+++ src/WAVI.jl
@@ -505,8 +505,8 @@ one grid point where ice grounded and one where ice floating.
 """
 function get_glx(wavi)
       glmask=diff(sign.(wavi.gh.haf),dims=1).==-2 #calculate where sign of height above floating passes thru zero
-      glx1=wavi.gh.xx[1:end-1,:][glmask] #x co-ordiates upstream of grounding line
-      glx2=wavi.gh.xx[2:end,:][glmask] #x co-ordinates immediately downstream
+      glx1=wavi.grid.xxh[1:end-1,:][glmask] #x co-ordiates upstream of grounding line
+      glx2=wavi.grid.xxh[2:end,:][glmask] #x co-ordinates immediately downstream
       haf1=wavi.gh.haf[1:end-1,:][glmask] #Height above floatation immediately upstream of gl
       haf2=wavi.gh.haf[2:end,:][glmask]
       glx=glx1+(glx2-glx1).*(zero(haf1)-haf1)./(haf2-haf1) #interpolate between grid points to find grounding line
