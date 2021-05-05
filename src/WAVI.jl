d0daeb3a537919bb00470934cbe1f00b6ef7f3ba
Alex Bradley <36222451+alextbradley@users.noreply.github.com>
2021-05-05 12:48:01 +0100

    moved initial conditions check to its own function

--- src/WAVI.jl
+++ src/WAVI.jl
@@ -507,31 +507,7 @@ function start(grid;
     initial_conditions = InitialConditions())
 
     #Check initial conditions, and revert to default values if not
-    if initial_conditions.initial_thickness == zeros(10,10)
-        default_thickness = params.default_thickness
-        println("Did not find a specified initial thickness, reverting to constant value specified in params ($default_thickness m everywhere)")
-        initial_conditions = @set initial_conditions.initial_thickness =  default_thickness*ones(grid.nx, grid.ny)
-    end
-    
-    if initial_conditions.initial_viscosity == zeros(10,10)
-        default_viscosity = params.default_viscosity
-        println("Did not find a specified initial viscosity, reverting to constant value specified in params ($default_viscosity everywhere) - units?!")
-        initial_conditions = @set initial_conditions.initial_viscosity =  default_viscosity*ones(grid.nx, grid.ny)
-    end
-
-    if initial_conditions.initial_temperature == zeros(10,10)
-        default_temperature = params.default_temperature
-        println("Did not find a specified initial temperature, reverting to constant value specified in params ($default_temperature K everywhere)")
-        initial_conditions = @set initial_conditions.initial_temperature =  default_temperature*ones(grid.nx, grid.ny)
-    end
-
-    if initial_conditions.initial_damage == zeros(10,10)
-        default_damage = params.default_damage
-        println("Did not find a specified initial damage field, reverting to constant value specified in params ($default_damage K everywhere)")
-        initial_conditions = @set initial_conditions.initial_damage =  default_damage*ones(grid.nx, grid.ny)
-    end
-
-
+    initial_conditions = check_initial_conditions(initial_conditions, params, grid)
 
     #Define masks for points on h-, u-, v- and c-grids that lie in model domain.
     h_mask = grid.h_mask 
@@ -617,6 +593,38 @@ function start(grid;
     wavi=State(grid,params,timestepping_params,solver_params,initial_conditions,gh,gu,gv,gc,g3d,wu,wv,clock)
 
     return wavi
+end
+
+"""
+    check_initial_conditions(initial_conditions, params)
+
+Check whether initial conditions have been specified. Default them to standard values if not
+"""
+function check_initial_conditions(initial_conditions::InitialConditions, params::Params, grid::Grid)
+    if initial_conditions.initial_thickness == zeros(10,10)
+        default_thickness = params.default_thickness
+        println("Did not find a specified initial thickness, reverting to constant value specified in params ($default_thickness m everywhere)")
+        initial_conditions = @set initial_conditions.initial_thickness =  default_thickness*ones(grid.nx, grid.ny)
+    end
+    
+    if initial_conditions.initial_viscosity == zeros(10,10)
+        default_viscosity = params.default_viscosity
+        println("Did not find a specified initial viscosity, reverting to constant value specified in params ($default_viscosity everywhere) - units?!")
+        initial_conditions = @set initial_conditions.initial_viscosity =  default_viscosity*ones(grid.nx, grid.ny)
+    end
+
+    if initial_conditions.initial_temperature == zeros(10,10)
+        default_temperature = params.default_temperature
+        println("Did not find a specified initial temperature, reverting to constant value specified in params ($default_temperature K everywhere)")
+        initial_conditions = @set initial_conditions.initial_temperature =  default_temperature*ones(grid.nx, grid.ny)
+    end
+
+    if initial_conditions.initial_damage == zeros(10,10)
+        default_damage = params.default_damage
+        println("Did not find a specified initial damage field, reverting to constant value specified in params ($default_damage everywhere)")
+        initial_conditions = @set initial_conditions.initial_damage =  default_damage*ones(grid.nx, grid.ny)
+    end
+    return initial_conditions
 end
 
 """
