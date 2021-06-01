c745c3f3020f42301a7e54b84a4fa045c871f493
Alex Bradley <36222451+alextbradley@users.noreply.github.com>
2021-06-01 13:11:21 +0100

    initial conditions updates
    improved (clearer) error messages. InitialConditions defaults to single NaN entry if no argument is passed (rather than somewhat arbitrary zeros(10,10)) -- these fix issues in #10

--- src/InitialConditions.jl
+++ src/InitialConditions.jl
@@ -1,7 +1,7 @@
-#structure to store initial conditions
+#structure to store initial conditions. We default values are a 1x1 nan matrix. Unspecified initial conditions are overwritten in Model constructor to default values passed via Params structure
 @with_kw struct InitialConditions{T <: Real}
-    initial_thickness::Array{T,2} = zeros(10,10)
-    initial_viscosity::Array{T,2} = zeros(10,10) #placeholder array
-    initial_temperature::Array{T,2} = zeros(10,10)
-    initial_damage::Array{T,2} = zeros(10,10)
+    initial_thickness::Array{T,2} = fill!(Array{Float64}(undef,1,1),NaN)
+    initial_viscosity::Array{T,2} = fill!(Array{Float64}(undef,1,1),NaN)
+    initial_temperature::Array{T,2} = fill!(Array{Float64}(undef,1,1),NaN)
+    initial_damage::Array{T,2} = fill!(Array{Float64}(undef,1,1),NaN)
 end
--- src/Models/model_utilities.jl
+++ src/Models/model_utilities.jl
@@ -5,27 +5,27 @@ check_initial_conditions(initial_conditions, params)
 Check whether initial conditions have been specified. Default them to standard values if not
 """
 function check_initial_conditions(initial_conditions, params, grid)
-if initial_conditions.initial_thickness == zeros(10,10)
+if all(isnan.(initial_conditions.initial_thickness))
     default_thickness = params.default_thickness
-    println("Did not find a specified initial thickness, reverting to default value specified in params ($default_thickness m everywhere)")
+    @info "Did not find a specified initial thickness, reverting to default value specified in params ($default_thickness m everywhere)...\n...If you have set niter0 > 0 without invoking the update flag, you can ignore this message"
     initial_conditions = @set initial_conditions.initial_thickness =  default_thickness*ones(grid.nx, grid.ny)
 end
 
-if initial_conditions.initial_viscosity == zeros(10,10)
+if all(isnan.(initial_conditions.initial_viscosity))
     default_viscosity = params.default_viscosity
-    println("Did not find a specified initial viscosity, reverting to default value specified in params ($default_viscosity Pa s everywhere)")
+    @info "Did not find a specified initial viscosity, reverting to default value specified in params ($default_viscosity Pa s everywhere)...\n...If you have set niter0 > 0 without invoking the update flag, you can ignore this message"
     initial_conditions = @set initial_conditions.initial_viscosity =  default_viscosity*ones(grid.nx, grid.ny)
 end
 
-if initial_conditions.initial_temperature == zeros(10,10)
+if all(isnan.(initial_conditions.initial_temperature))
     default_temperature = params.default_temperature
-    println("Did not find a specified initial temperature, reverting to default value specified in params ($default_temperature K everywhere)")
+    @info "Did not find a specified initial temperature, reverting to default value specified in params ($default_temperature K everywhere)...\n...If you have set niter0 > 0 without invoking the update flag, you can ignore this message"
     initial_conditions = @set initial_conditions.initial_temperature =  default_temperature*ones(grid.nx, grid.ny)
 end
 
-if initial_conditions.initial_damage == zeros(10,10)
+if all(isnan.(initial_conditions.initial_damage))
     default_damage = params.default_damage
-    println("Did not find a specified initial damage field, reverting to default value specified in params ($default_damage everywhere)")
+    @info "Did not find a specified initial damage field, reverting to default value specified in params ($default_damage Pa s everywhere)...\n...If you have set niter0 > 0 without invoking the update flag, you can ignore this message"
     initial_conditions = @set initial_conditions.initial_damage =  default_damage*ones(grid.nx, grid.ny)
 end
 return initial_conditions
