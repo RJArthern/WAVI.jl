e205b5e826091ea8beb2028a84ceb563785d581c
Alex Bradley <36222451+alextbradley@users.noreply.github.com>
2021-06-08 17:01:47 +0100

    Update SigmaGrid.jl
    added comments

--- src/Fields/SigmaGrid.jl
+++ src/Fields/SigmaGrid.jl
@@ -1,4 +1,36 @@
-#Struct to hold information on 3d-grid, (extends h-grid to multiple sigma levels).
+"""
+SigmaGrid(;
+        Nx,
+        Ny,
+        Nσ,
+        σ = collect(range(0.0,length=Nσ,stop=1.0)),
+        ζ = one(eltype(σ)) .- σ 
+        quadrature_weights = [0.5;ones(Nσ-2);0.5]/(Nσ-1)
+        η,
+        θ,
+        Φ,
+        glen_b
+        )
+
+Construct a WAVI.jl SigmaGrid with size (Nx,Ny,Nσ)
+SigmaGrid stores fields that are defined on the problem's three dimensional Sigma grid. 
+
+
+Keyword arguments
+=================
+- 'Nx': (required) Number of grid cells in x-direction in SigmaGrid (should be same as grid.nx)
+        Note that we store the grid size here, even though it can be easily inferred from grid, to increase transparency in velocity solve.
+- 'Ny': (required) Number of grid cells in y-direction in SigmaGrid (should be same as grid.ny )
+- 'Nσ': (required) Number of grid cells in the vertical
+- 'σ' : Vertical levels
+- 'ζ' : Reverse vertical levels
+- 'quadrature_weights' : weights associated with sigma levels, used in computation of integrals over thickness
+- 'η' : (required) three dimensional viscosity field
+- 'θ' : (required) three dimensional temperature field
+- 'Φ' : (required) three dimensional damage field
+- 'glen_b': (required) three dimensional field of glen_b values in viscosity calcluations
+"""
+
 @with_kw struct SigmaGrid{T <: Real, N <: Integer}
 Nx :: N
 Ny :: N
@@ -11,42 +43,3 @@ quadrature_weights :: Vector{T} = [0.5;ones(Nσ-2);0.5]/(Nσ-1); @assert length(
 Φ :: Array{T,3}; @assert size(Φ)==(Nx,Ny,Nσ)
 glen_b :: Array{T,3} = glen_b.(θ,Φ); @assert size(glen_b)==(Nx,Ny,Nσ)
 end
-
-#struct SigmaGrid{T <: Real, N <: Integer}
-#                    Nx :: N
-#                    Ny :: N
-#                    Nσ :: N
-#                     σ :: Vector{T} 
-#                     ζ :: Vector{T} 
-#  quadrature_weights :: Vector{T} 
-#                    η :: Array{T,3} 
-#                     θ :: Array{T,3} 
-#                     Φ :: Array{T,3} 
-#                glen_b :: Array{T,3}   #
-#end
-
-#function SigmaGrid(;
-#                    Nx,
-#                    Ny,
-#                    Nσ,
-#                    quadrature_weights = [0.5;ones(Nσ-2);0.5]/(Nσ-1),
-#                    η,
-#                    θ,
-#                    Φ,
-#                    glen_b)
-
-#σ = collect(range(0.0,length=Nσ,stop=1.0)); @assert length(σ) == Nσ
-#ζ = one(eltype(σ)) .- σ ; @assert length(ζ) == Nσ
-
-#return SigmaGrid(
-#                Nx,
-#                Ny,
-#                Nσ,
-#                σ,
-#                ζ,
-#                quadrature_weights,
-#                η, 
-#                θ,
-#                Φ,
-#                glen_b)
-#end
