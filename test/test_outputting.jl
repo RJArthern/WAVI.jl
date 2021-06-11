07e12b33b505b990f6ab579ad4f05efff5128aaf
Alex Bradley <36222451+alextbradley@users.noreply.github.com>
2021-06-11 16:39:14 +0100

    Update test_outputting.jl
    added tests for pickups

--- test/test_outputting.jl
+++ test/test_outputting.jl
@@ -1,15 +1,28 @@
 using Test, WAVI, MAT, JLD2
 
-function output_test(; dt  = 0.5, end_time = 100., output_freq = 5., output_format = "jld2", zip_format = "none", prefix = "outfile", dump_vel = false, output_path = ".")
+function output_test(; dt  = 0.5, 
+                    end_time = 100., 
+                    output_freq = 5., 
+                    output_format = "jld2", 
+                    zip_format = "none", 
+                    prefix = "outfile", 
+                    dump_vel = false, 
+                    output_path = ".",
+                    pchkpt_freq = Inf,
+                    chkpt_freq = Inf,
+                    niter0 = 0)
+
     grid = Grid() #default grid with nx = 80, ny = 10
     bed = WAVI.mismip_plus_bed
     solver_params = SolverParams(maxiter_picard = 1)
     model = Model(grid = grid, bed_elevation = bed, solver_params = solver_params)
-    timestepping_params = TimesteppingParams(dt = dt, end_time = end_time)
+    timestepping_params = TimesteppingParams(niter0 = niter0, dt = dt, end_time = end_time, chkpt_freq = chkpt_freq, pchkpt_freq = pchkpt_freq)
+
     outputs = (h = model.fields.gh.h,
                 u = model.fields.gh.u,
                 v = model.fields.gh.v,
                 b = model.fields.gh.b) #output velocities and thickness
+
     output_freq = output_freq
     output_params = OutputParams(outputs = outputs, 
                         output_freq = output_freq,
@@ -29,6 +42,7 @@ function output_test(; dt  = 0.5, end_time = 100., output_freq = 5., output_form
 end
 
 @testset "Outputting" begin
+    if false
     @testset "Output files" begin 
         @info "Testing outputting..."
         
@@ -102,6 +116,7 @@ end
         u = ncread(fname, "u");
         v = ncread(fname, "v")
         b = ncread(fname, "b")
+        t = ncread(fname, "t")
         @test ncread(fname, "TIME") == 5.:5.:100.
         @test size(x) == (80,)
         @test size(y) == (10,)
@@ -113,6 +128,108 @@ end
         #delete the folder
         rm(folder, force = true, recursive = true)
         end
+
+    end
+    end
+
+    @testset "Outputting after a restart" begin 
+        @info "Testing outputting after a restart"
+        
+        for output_format in ["mat", "jld2"]
+
+        folder = "outputs/"
+        isdir(folder) && rm(folder, force = true, recursive = true) #make a new folder
+        mkdir(folder)
+
+        #run simulation first time with no zipping
+        output_test(; dt  = 0.5, 
+                    end_time = 10., 
+                    output_freq = 0.5, 
+                    output_format = output_format, 
+                    output_path = folder,
+                    zip_format = "nc", 
+                    dump_vel = false, 
+                    pchkpt_freq = 1.)
+
+        #run again with different niter0
+        sim = output_test(; dt  = 0.5, 
+                    end_time = 20., 
+                    output_freq = 0.5, 
+                    niter0 = 20,
+                    output_format = output_format, 
+                    output_path = folder,
+                    zip_format = "nc", 
+                    dump_vel = false, 
+                    pchkpt_freq = 1.)
+
+        
+        #test variables read from nc file
+        fname = string(folder, sim.output_params.prefix, ".nc")
+        println(fname)
+        x = ncread(fname, "x")
+        y = ncread(fname, "y")
+        h = ncread(fname, "h")
+        u = ncread(fname, "u")
+        v = ncread(fname, "v")
+        b = ncread(fname, "b")
+        t = ncread(fname, "TIME")
+        @test ncread(fname, "TIME") == 0.5:0.5:20.
+        @test size(x) == (80,)
+        @test size(y) == (10,)
+        @test size(b) == (length(x),length(y),length(t))
+        @test size(u) == (length(x),length(y),length(t))
+        @test size(v) == (length(x),length(y),length(t))
+        @test size(h) == (length(x),length(y),length(t))
+
+        #check we have the right number of output files 
+        foldersim = sim.output_params.output_path 
+        @test foldersim[end] == '/' #test that we do have the / at end of path
+        files = [string(foldersim,f) for f in  readdir(foldersim) if endswith(f, output_format)]
+        println(files)
+        @test length(files) == 40    #check there are the correct number of output files
+         
+        #now repeat for just one timestep
+        sim = output_test(; dt  = 0.5, 
+                        end_time = 20.5, 
+                        output_freq = 0.5, 
+                        niter0 = 40,
+                        output_format = output_format, 
+                        output_path = folder,
+                        zip_format = "nc", 
+                        dump_vel = false, 
+                        pchkpt_freq = 1.)
+         
+        #test variables read from nc file
+        fname = string(folder, sim.output_params.prefix, ".nc")
+        println(fname)
+        x = ncread(fname, "x")
+        y = ncread(fname, "y")
+        h = ncread(fname, "h")
+        u = ncread(fname, "u")
+        v = ncread(fname, "v")
+        b = ncread(fname, "b")
+        t = ncread(fname, "TIME")
+        @test ncread(fname, "TIME") == 0.5:0.5:20.5
+        @test size(x) == (80,)
+        @test size(y) == (10,)
+        @test size(b) == (length(x),length(y),length(t))
+        @test size(u) == (length(x),length(y),length(t))
+        @test size(v) == (length(x),length(y),length(t))
+        @test size(h) == (length(x),length(y),length(t))
+
+        #check we have the right number of files
+        foldersim = sim.output_params.output_path 
+        @test foldersim[end] == '/' #test that we do have the / at end of path
+        files = [string(foldersim,f) for f in  readdir(foldersim) if endswith(f, output_format)]
+        println(files)
+        @test length(files) == 41    #check there are the correct number of output files
+
+        # delete everything you just made
+        rm(folder, force = true, recursive = true)
+        foreach(rm, filter(endswith(".mat"), readdir()))
+        foreach(rm, filter(endswith(".jld2"), readdir()))
+        end
+
 
     end
 end
