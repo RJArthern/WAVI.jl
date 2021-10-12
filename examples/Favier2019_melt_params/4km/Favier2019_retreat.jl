5b79c73bb3916c22e56658c9a2ff74a2e7bdf81d
Alex Bradley <36222451+alextbradley@users.noreply.github.com>
2021-10-12 16:08:36 +0100

    update init and retreat codes for Favier 2019

--- examples/Favier2019_melt_params/4km/Favier2019_retreat.jl
+++ examples/Favier2019_melt_params/4km/Favier2019_retreat.jl
@@ -1,6 +1,6 @@
 using WAVI, Plots
 """
-Produce a plot of the melt rate in MISMIP for specified melt rate parametrization (c.f. figure 4 in Favier 2019 10.5194/gmd-12-2255-2019)
+Run retreat experiment (100 years from MISMIP steady state) 
 ***Options***
     - PICO_nboxP_zQ    : PICO with P boxes, ambient temperature taken at Qm depth for P = 10, 8, 5, 2 and Q = 500, 700
                         (e.g. "PICO_nbox2_z700")
@@ -8,9 +8,9 @@ Produce a plot of the melt rate in MISMIP for specified melt rate parametrizatio
     - MISMIP_1r        : Melt rate according to the MISMIP+ ice 1r experiment (doi: 10.5194/tc-14-2283-2020) scaled to match the mean melt rate
 """
 
-melt_rate_model = "MISMIP_1r"
+melt_rate_model = "PME"
 
-function Favier2019_4km_init(melt_model)
+function Favier2019_4km_retreat(melt_model)
 # Grid and boundary conditions
 nx = 160
 ny = 20
@@ -50,31 +50,46 @@ model = Model(grid=grid,
             bed_elevation=bed,
             melt_rate = melt_model, 
             initial_conditions=initial_conditions,
-            solver_params=SolverParams(maxiter_picard=1) 
+            solver_params=SolverParams(maxiter_picard=10) ,
+            params=Params(accumulation_rate=0.3)
             ) ;
 
-# update the configuration
-update_state!(model)
+#timestepping parameters
+niter0 = 0
+dt = 0.02
+end_time = 10.
+chkpt_freq = 0.5
+pchkpt_freq = 0.5
+timestepping_params = TimesteppingParams(niter0 = niter0, 
+                                        dt = dt, 
+                                        end_time = end_time, 
+                                        chkpt_freq = chkpt_freq, 
+                                        pchkpt_freq = pchkpt_freq)
 
-#contour plot melt rate
-m = deepcopy(model.fields.gh.basal_melt)
-m[model.fields.gh.grounded_fraction .== 1.] .= NaN
-msat = deepcopy(m)
-msat[msat .> 50] .= 50
-#msat[zb .> -300] .= NaN;
-x =  model.grid.xxh[:,1]; y = model.grid.yyh[1,:];
-plt = heatmap(x ./ 1e3,y / 1e3, msat', 
-                fill=true, 
-                linewidth=0, 
-                colorbar=true,
-                colorbar_title="melt rate (m/yr)",
-                framestyle=:box)
+#output parameters
+folder =  joinpath(dirname(@__FILE__),"retreat_output")
+isdir(folder) && rm(folder, force = true, recursive = true)
+mkdir(folder) #make a clean folder for outputs
+outputs = (h   = model.fields.gh.h,
+            u  = model.fields.gh.u,
+            v  = model.fields.gh.v,
+            melt = model.fields.gh.basal_melt,
+            grfrac = model.fields.gh.grounded_fraction)
+output_freq = 0.5
+output_params = OutputParams(outputs = outputs, 
+                        output_freq = output_freq,
+                        output_format = "jld2",
+                        output_path = folder,
+                        zip_format = "nc")
 
-xlims!((420, 640))
-xlabel!("x (km)")
-ylabel!("y (km)")
+simulation = Simulation(model = model, 
+                    timestepping_params = timestepping_params, 
+                    output_params = output_params);
+        
+#perform the simulation
+run_simulation!(simulation);
 
-return model, plt
+return simulation
 end
 
 #Need these for PICO 
@@ -118,17 +133,17 @@ elseif melt_rate_model == "PME"
     melt_model = PlumeEmulator(α=1.49)
 
 elseif melt_rate_model == "MISMIP_1r"
-    melt_model = MISMIPMeltRateOne(α = 0.184)
+    melt_model = MISMIPMeltRateOne(α = 0.32)
 else
     throw(ArgumentError("Specified melt rate model not found"))
 end
 
 
 
-model, plt = Favier2019_4km_init(melt_model);
+@time simulation = Favier2019_4km_retreat(melt_model);
 
-m = deepcopy(model.fields.gh.basal_melt);
-zb = model.fields.gh.b .* (model.fields.gh.grounded_fraction .== 1) + -  918.0 / 1028.0  .* model.fields.gh.h .* (model.fields.gh.grounded_fraction .< 1)
-idx = (model.fields.gh.grounded_fraction .== 0)
-mean_melt = sum(m[idx])./length(m[idx])
-println("mean melt rate for shelf points is $mean_melt")
+#m = deepcopy(model.fields.gh.basal_melt);
+#zb = model.fields.gh.b .* (model.fields.gh.grounded_fraction .== 1) + -  918.0 / 1028.0  .* model.fields.gh.h .* (model.fields.gh.grounded_fraction .< 1)
+#idx = (model.fields.gh.grounded_fraction .== 0)
+#mean_melt = sum(m[idx])./length(m[idx])
+#println("mean melt rate for shelf points is $mean_melt")
