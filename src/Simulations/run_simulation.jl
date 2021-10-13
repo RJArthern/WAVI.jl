a7e14b839fb37eb0ae0cb7668471c7dd8f2e1794
CRosieWilliams <chll1@bas.ac.uk>
2021-10-13 13:39:33 +0100

    Revert "Merge pull request #41 from RJArthern/AlexDev_CRWtest"
    This reverts commit 410faeb48d8953c63c7ea704c3545b56f70e5c9e, reversing
    changes made to 1dda6c3721138d495a150468acee342fad8adedf.

--- src/Simulations/run_simulation.jl
+++ src/Simulations/run_simulation.jl
@@ -10,31 +10,17 @@ function timestep!(simulation)
         update_thickness!(simulation)
     end
     update_clock!(simulation)
-    
 end
 
 """
 update_thickness!(model::AbstractModel)
 
 Update thickness using rate of change of thickness and apply minimum thickness constraint.
-Includes an option for not evolving shelf thickness
 """
 function update_thickness!(simulation)
 @unpack model,timestepping_params=simulation
-hUpdate=zeros(model.grid.nx,model.grid.ny)
-aground=zeros(model.grid.nx,model.grid.ny)
-hUpdate[model.fields.gh.mask]=max.(model.params.minimum_thickness .- model.fields.gh.h[model.fields.gh.mask],timestepping_params.dt*model.fields.gh.dhdt[model.fields.gh.mask])
-#Specify whether to evolve the shelves:
-if !model.params.evolveShelves
-    hUpdate[model.fields.gh.mask]=max.(model.params.smallHAF.-(model.params.density_ocean./model.params.density_ice).*model.fields.gh.b[model.fields.gh.mask].-model.fields.gh.h[model.fields.gh.mask],hUpdate[model.fields.gh.mask])
-    aground=(model.fields.gh.haf.>=0)
-    wc=[1 1 1; 1 1 1; 1 1 1]
-    w=centered(wc)
-    nearfloat_mask=imfilter(model.fields.gh.mask.&.!aground,reflect(w),Fill(0,w))
-    nearfloat_mask=iszero.(iszero.(nearfloat_mask))
-    hUpdate[nearfloat_mask].=0
-  end
-model.fields.gh.h[model.fields.gh.mask] = model.fields.gh.h[model.fields.gh.mask] .+ hUpdate[model.fields.gh.mask]
+onesvec=ones(model.grid.nx*model.grid.ny)
+model.fields.gh.h[model.fields.gh.mask] = model.fields.gh.h[model.fields.gh.mask] .+ max.(model.params.minimum_thickness .- model.fields.gh.h[model.fields.gh.mask],timestepping_params.dt*model.fields.gh.dhdt[model.fields.gh.mask])
 return simulation
 end
 
@@ -47,7 +33,6 @@ function update_clock!(simulation)
     @unpack clock,timestepping_params=simulation
     clock.n_iter += 1
     clock.time += timestepping_params.dt
-    println("The time is ", clock.time)
     return simulation
 end
 
