afee210891405ceab812061d3411398ad7e58731
David-Bett4 <59053177+David-Bett4@users.noreply.github.com>
2021-11-25 10:19:48 +0000

    Added initial_u and initial_v as IC options

--- src/InitialConditions.jl
+++ src/InitialConditions.jl
@@ -3,7 +3,9 @@ InitialConditions(;
                     initial_thickness = fill!(Array{Float64}(undef,1,1),NaN),
                     initial_viscosity = fill!(Array{Float64}(undef,1,1),NaN),
                     initial_temperature = fill!(Array{Float64}(undef,1,1),NaN),
-                    initial_damage = fill!(Array{Float64}(undef,1,1),NaN))
+                    initial_damage = fill!(Array{Float64}(undef,1,1),NaN)),
+                    initial_u = fill!(Array{Float64}(undef,1,1),NaN)),
+                    initial_v = fill!(Array{Float64}(undef,1,1),NaN))
 
 Construct a WAVI.jl initial conditions object. 
 Unpassed arguments default to 1x1 nan matrix; unspecified initial conditions are overwritten by default values specified in Params structure when model is assembled.
@@ -15,10 +17,14 @@ Keyword arguments
 - 'initial_viscosity': (nx x ny x nz) matrix defining viscosity on sigma levels at t = 0
 - 'initial_temperature': (nx x ny) matrix defining temperature on sigma levels at t = 0
 - 'initial_damage': (nx x ny) matrix defining depth averaged damage at t = 0
+- 'initial_u': (nxu x nyu) matrix defining ice velocity in x direction at t = 0
+- 'initial_v': (nxv x nyv) matrix defining ice velocity in y direction at t = 0
 """
 @with_kw struct InitialConditions{T <: Real}
     initial_thickness::Array{T,2} = fill!(Array{Float64}(undef,1,1),NaN)
     initial_viscosity::Array{T,3} = fill!(Array{Float64}(undef,1,1,1),NaN)
     initial_temperature::Array{T,3} = fill!(Array{Float64}(undef,1,1,1),NaN)
     initial_damage::Array{T,3} = fill!(Array{Float64}(undef,1,1,1),NaN)
+    initial_u::Array{T,2} = fill!(Array{Float64}(undef,1,1),NaN)
+    initial_v::Array{T,2} = fill!(Array{Float64}(undef,1,1),NaN)
 end
