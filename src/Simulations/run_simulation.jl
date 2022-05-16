c65267c1ad73b6d386ef8cae7d962c5e7ff08fba
David-Bett4 <59053177+David-Bett4@users.noreply.github.com>
2022-05-16 14:09:07 +0100

    Changes to timestamps for U,V and float coupling outputs
    Reduced the timestamp for these coupling variables by 1 timestep, as these are generated before thickness is time stepped.

--- src/Simulations/run_simulation.jl
+++ src/Simulations/run_simulation.jl
@@ -192,7 +192,8 @@ Write the velocity at the the final timestep of the simulation (used in the coup
 function write_vel(simulation::Simulation,h_out_line_w,h_out_line_e,h_out_line_n,h_out_line_s,x_w,x_e,y_s,y_n)
     @unpack model, output_params = simulation  
     
-    clock_time_C=Int(round((simulation.clock.time*(3600*24*365))/output_params.dt_coup))
+    clock_time_C=Int(round(((simulation.clock.time - simulation.timestepping_params.dt)*(3600*24*365))/output_params.dt_coup))
+    clock_time_Cn=Int(round((simulation.clock.time*(3600*24*365))/output_params.dt_coup))
         
     uVel_file_string = string(simulation.output_params.prefix, "_U", lpad(clock_time_C, 10,"0") ,".bin")
     vVel_file_string = string(simulation.output_params.prefix, "_V", lpad(clock_time_C, 10,"0") ,".bin")
@@ -252,7 +253,7 @@ function write_vel(simulation::Simulation,h_out_line_w,h_out_line_e,h_out_line_n
      h_out_b .= hton.(h_out_b)
      
         
-     hb_file_string = string(simulation.output_params.prefix, "_Hb", lpad(clock_time_C, 10,"0") ,".bin")
+     hb_file_string = string(simulation.output_params.prefix, "_Hb", lpad(clock_time_Cn, 10,"0") ,".bin")
     
      hbfileID =  open(hb_file_string,"w")
      write(hbfileID, h_out_b[:,:])
