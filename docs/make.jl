199914cd049c7e72a3000165f9484fc941315f57
Alex Bradley <36222451+alextbradley@users.noreply.github.com>
2022-09-15 10:21:05 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,9 @@
 push!(LOAD_PATH,"../src/")
+import Pkg; Pkg.add("Documenter")
+import Pkg; Pkg.add("DocumenterCitations")
+import Pkg; Pkg.add("Literate")
+import Pkg; Pkg.add("Plots")
+
 using Documenter
 using DocumenterCitations
 using Literate 
