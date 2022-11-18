20a1e480de94263e4e9a612eb0dba0130d65a314
Alex Bradley <alextbradley.1@gmail.com>
2022-11-18 11:00:15 +0000

    Update run_simulation.jl

--- src/Simulations/run_simulation.jl
+++ src/Simulations/run_simulation.jl
@@ -22,7 +22,7 @@ update_thickness!(model::AbstractModel)
 Update thickness using rate of change of thickness and apply minimum thickness constraint. Includes an option for not evolving shelf thickness.
 """
 function update_thickness!(simulation)
-@unpack model,timestepping_params,solver_params=simulation
+@unpack model,timestepping_params=simulation
 hUpdate=zeros(model.grid.nx,model.grid.ny)
 aground=zeros(model.grid.nx,model.grid.ny)
 hUpdate[model.fields.gh.mask]=max.(model.params.minimum_thickness .- model.fields.gh.h[model.fields.gh.mask],timestepping_params.dt*model.fields.gh.dhdt[model.fields.gh.mask])
@@ -31,7 +31,7 @@ if !model.params.evolveShelves
     hUpdate[model.fields.gh.mask]=max.(model.params.smallHAF.-(model.params.density_ocean./model.params.density_ice).*model.fields.gh.b[model.fields.gh.mask].-model.fields.gh.h[model.fields.gh.mask],hUpdate[model.fields.gh.mask])
     aground=(model.fields.gh.haf.>=0)
 
-    wc=map(Int, ones(solver_params.no_update_shelves_mask_size,solver_params.no_update_shelves_mask_size)) # N x N matrix of ones, which specifies the mask size
+    wc=map(Int, ones(model.solver_params.no_update_shelves_mask_size,model.solver_params.no_update_shelves_mask_size)) # N x N matrix of ones, which specifies the mask size
     w=centered(wc) #centre the mask
     nearfloat_mask=imfilter(model.fields.gh.mask.&.!aground,reflect(w),Fill(0,w)) 
     nearfloat_mask=iszero.(iszero.(nearfloat_mask))
