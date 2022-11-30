c7daea9c8ac7e9a32cf32003796dcdcd22925ae4
David-Bett4 <59053177+David-Bett4@users.noreply.github.com>
2022-11-30 11:09:00 +0000

    Changes discretisation of shear stresses

--- src/Models/update_velocities.jl
+++ src/Models/update_velocities.jl
@@ -286,9 +286,10 @@ end
 Precompute various diagonal matrices used in defining the momentum operator.
 """
 function update_rheological_operators!(model::AbstractModel)
-    @unpack gh,gu,gv=model.fields
+    @unpack gh,gu,gv,gc=model.fields
     @unpack params=model
     gh.dneghηav[] .= gh.crop*Diagonal(-gh.h[:].*gh.ηav[:])*gh.crop
+    gc.dneghηav[] .= gc.crop*Diagonal(-gh.cent_xy*(gh.h[:].*gh.ηav[:]))*gc.crop
     gu.dnegβeff[] .= gu.crop*Diagonal(-gu.βeff[:])*gu.crop
     gv.dnegβeff[] .= gv.crop*Diagonal(-gv.βeff[:])*gv.crop
     gh.dimplicit[] .= gh.crop*Diagonal(-params.density_ice * params.g * params.dt * gh.dsdh[:])*gh.crop
